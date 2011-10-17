7981440545103fdda9c4dfbe49bcd46e2a3b91d3
Vasiliy Tsubenko <vasayxtx@gmail.com>
2011-10-18 03:05:24 +1100

    Refactoring code

--- OpenMP/problem2.c
+++ OpenMP/problem2.c
@@ -1,107 +1,44 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-#include <sys/time.h>
-#include <omp.h>
-#include <math.h>
-#include <string.h>
-
+#include "p_main.h"
 #include "quicksort.h"
 
-#define _MAX_RAND (int)pow(10, 6)
+#define SINGLE_TH "Quicksort. Single thread"
+#define MULTI_TH_SEC "Quicksort. Multi threads using nested parallel regions"
+#define MULTI_TH_TASK "Quicksort. Multi threads using tasks from OpenMP 3.0"
 
-#define BOOL int
-#define TRUE 1
-#define FALSE 0
-
-void run_sort(int *arr, size_t n, void (*f)(int *, size_t), char *title);
+void handle(int argc, char *argv[], int *arr, size_t n);
 
 int main(int argc, char *argv[])
 {
-  int i;
-  size_t n;
-  int *arr;
-  FILE *f_in, *f_out;
+  return run_main(argc, argv, handle);
+}
 
-  BOOL is_file = FALSE;
-
-  if (argc < 3) return EXIT_FAILURE;
-
-  if (!strcmp(argv[1], "-r"))
-  {
-    /*Randomize data for computation*/
-    n = atoi(argv[2]);
-
-    arr = (int *)malloc(n * sizeof(int));
-    if (!arr) return EXIT_FAILURE;
-
-    for (i = 0; i < n; ++i) arr[i] = rand() % _MAX_RAND;
-  }
-  else
-  {
-    /*Read data from input file (argv[1]) and write result to output file (argv[2])*/
-    char *name_f_in = argv[1];
-    char *name_f_out = argv[2];
-
-    f_in = fopen(name_f_in, "r");
-    f_out = fopen(name_f_out, "w");
-    if (!f_in || !f_out) return EXIT_FAILURE;
-
-    fscanf(f_in, "%i", &n);
-
-    arr = (int *)malloc(n * sizeof(int));
-    if (!arr) return EXIT_FAILURE;
-
-    for (i = 0; i < n; ++i)
-      fscanf(f_in, "%i", &arr[i]);
-
-    is_file = TRUE;
-  }
-
+void handle(int argc, char *argv[], int *arr, size_t n)
+{
   if (argc < 4)
-    run_qsort(arr, n, sqsort, "Single thread");
+  {
+    run_sort(arr, n, sqsort, SINGLE_TH);
+  }
   else if (!strcmp(argv[3], "-ms"))
-    run_qsort(arr, n, pqsort_sections, "Multi threads using nested parallel regions");
+  {
+    run_sort(arr, n, pqsort_sections, MULTI_TH_SEC);
+  }
   else if (!strcmp(argv[3], "-mt"))
-    run_qsort(arr, n, pqsort_tasks, "Multi threads using tasks from OpenMP 3.0");
+  {
+    run_sort(arr, n, pqsort_tasks, MULTI_TH_TASK);
+  }
   else if (!strcmp(argv[3], "-a"))
   {
     int *arr_foo = (int *)malloc(n * sizeof(int));
     memcpy(arr_foo, arr, n * sizeof(int));
     int *arr_bar = (int *)malloc(n * sizeof(int));
     memcpy(arr_bar, arr, n * sizeof(int));
-    run_qsort(arr, n, sqsort, "Single thread");
-    run_qsort(arr_foo, n, pqsort_sections, "Multi threads using nested parallel regions");
-    run_qsort(arr_bar, n, pqsort_tasks, "Multi threads using tasks from OpenMP 3.0");
+
+    run_sort(arr, n, sqsort, SINGLE_TH);
+    run_sort(arr_foo, n, pqsort_sections, MULTI_TH_SEC);
+    run_sort(arr_bar, n, pqsort_tasks, MULTI_TH_TASK);
+
     free(arr_bar); free(arr_foo);
   }
-  else return EXIT_FAILURE;
-
-  if (is_file)
-  {
-    for (i = 0; i < n; ++i)
-      fprintf(f_out, "%i ", arr[i]);
-    fprintf(f_out, "\n\n");
-
-    fclose(f_in);
-    fclose(f_out);
-  }
-  
-  free(arr);
-
-  return EXIT_SUCCESS;
-}
-
-void run_qsort(int *arr, size_t n, void (*f)(int *, size_t), char *title)
-{
-  struct timeval t_begin, t_end;
-  float t_diff;
-
-  printf("%s\n", title);
-  gettimeofday(&t_begin, NULL);
-  (*f)(arr, n);
-  gettimeofday(&t_end, NULL);
-  t_diff = t_end.tv_sec - t_begin.tv_sec + 0.000001 * (t_end.tv_usec - t_begin.tv_usec);
-  printf("Work time of sorting at %f seconds\n", t_diff);
+  else exit(EXIT_FAILURE);
 }
 
--- OpenMP/problem3.c
+++ OpenMP/problem3.c
@@ -1,101 +1,36 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-#include <sys/time.h>
-#include <omp.h>
-#include <math.h>
-#include <string.h>
-
+#include "p_main.h"
 #include "mergesort.h"
 
-#define _MAX_RAND (int)pow(10, 6)
+#define SINGLE_TH "Mergesort. Single thread"
+#define MULTI_TH_SEC "Mergesort. Multi threads using nested parallel regions"
 
-#define BOOL int
-#define TRUE 1
-#define FALSE 0
-
-void run_mergesort(int *arr, size_t n, void (*f)(int *, size_t), char *title);
+void handle(int argc, char *argv[], int *arr, size_t n);
 
 int main(int argc, char *argv[])
 {
-  int i;
-  size_t n;
-  int *arr;
-  FILE *f_in, *f_out;
+  return run_main(argc, argv, handle);
+}
 
-  BOOL is_file = FALSE;
-
-  if (argc < 3) return EXIT_FAILURE;
-
-  if (!strcmp(argv[1], "-r"))
-  {
-    /*Randomize data for computation*/
-    n = atoi(argv[2]);
-
-    arr = (int *)malloc(n * sizeof(int));
-    if (!arr) return EXIT_FAILURE;
-
-    for (i = 0; i < n; ++i) arr[i] = rand() % _MAX_RAND;
-  }
-  else
-  {
-    /*Read data from input file (argv[1]) and write result to output file (argv[2])*/
-    char *name_f_in = argv[1];
-    char *name_f_out = argv[2];
-
-    f_in = fopen(name_f_in, "r");
-    f_out = fopen(name_f_out, "w");
-    if (!f_in || !f_out) return EXIT_FAILURE;
-
-    fscanf(f_in, "%i", &n);
-
-    arr = (int *)malloc(n * sizeof(int));
-    if (!arr) return EXIT_FAILURE;
-
-    for (i = 0; i < n; ++i)
-      fscanf(f_in, "%i", &arr[i]);
-
-    is_file = TRUE;
-  }
-
+void handle(int argc, char *argv[], int *arr, size_t n)
+{
   if (argc < 4)
-    run_mergesort(arr, n, smergesort, "Single thread");
+  {
+    run_sort(arr, n, smergesort, SINGLE_TH);
+  }
   else if (!strcmp(argv[3], "-m"))
-    run_mergesort(arr, n, pmergesort, "Multi threads using nested parallel regions");
+  {
+    run_sort(arr, n, pmergesort, MULTI_TH_SEC);
+  }
   else if (!strcmp(argv[3], "-a"))
   {
     int *arr_foo = (int *)malloc(n * sizeof(int));
     memcpy(arr_foo, arr, n * sizeof(int));
-    run_mergesort(arr, n, smergesort, "Single thread");
-    run_mergesort(arr_foo, n, pmergesort, "Multi threads using nested parallel regions");
+
+    run_sort(arr, n, smergesort, SINGLE_TH);
+    run_sort(arr_foo, n, pmergesort, MULTI_TH_SEC);
+
+    free(arr_foo);
   }
-  else return EXIT_FAILURE;
-
-  if (is_file)
-  {
-    for (i = 0; i < n; ++i)
-      fprintf(f_out, "%i ", arr[i]);
-    fprintf(f_out, "\n\n");
-
-    fclose(f_in);
-    fclose(f_out);
-  }
-  
-  free(arr);
-
-  return EXIT_SUCCESS;
-}
-
-void run_mergesort(int *arr, size_t n, void (*f)(int *, size_t), char *title)
-{
-  struct timeval t_begin, t_end;
-  float t_diff;
-
-  printf("%s\n", title);
-  gettimeofday(&t_begin, NULL);
-  (*f)(arr, n);
-  gettimeofday(&t_end, NULL);
-  t_diff = t_end.tv_sec - t_begin.tv_sec + 0.000001 * (t_end.tv_usec - t_begin.tv_usec);
-  printf("Work time of sorting at %f seconds\n", t_diff);
+  else exit(EXIT_FAILURE);
 }
 
